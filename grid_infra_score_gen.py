bcaa45d0ff3b7b166edd12f4e95b8e3d597f978e
Maximilian Herzog <maxi.herzog@t-online.de>
2020-03-25 03:56:00 +0100

    Added progressbars + additional info

--- grid_infra_score_gen.py
+++ grid_infra_score_gen.py
@@ -18,6 +18,22 @@ import overpy
 import sys
 import time
 
+### Used for displaying Progressbars
+# Shoutout to eusoubrasileiro on Stackoverflow!
+def progressbar(it, prefix="", size=60, file=sys.stdout):
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
+        file.flush()
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    file.write("\n")
+    file.flush()
+
+#Main method of generator
 def main():
     if len(sys.argv) != 4:
         print("missing arguments. Usage:")
@@ -57,19 +73,20 @@ def main():
     #resultstring = "[out:json][timeout:200];area[name=\"Heidelberg\"]->.searchArea;\n("
 
 
-    resultstring = "[out:json][timeout:200];\n("
+    resultstring = "[out:json][timeout:1000];\n("
     crs = {'init': 'epsg:4326'} #4326
     #df=pd.read_csv('tagweights.csv', sep=':',header=0)
     dict = {}
     print("Collecting dict: ")
+    n_cols = len(df.columns)-3
     for i in range(0,len(df.values)):
-        for x in range(2, 5):
+        for x in range(2, n_cols+2):
             string = str(df.values[i][x])
             if not string == "nan":
                 liststring = string.split(", ")
                 for value in liststring:
-                    dict[value] = df.values[i][6]
-                    print(value + " [" + str(df.values[i][6]) + "]")
+                    dict[value] = df.values[i][n_cols+2]
+                    print(df.columns[x] + ":" + value + " [" + str(df.values[i][n_cols+2]) + "]")
                     for pre in ["node", "way"]:
                         resultstring += "\t" + pre + '[\"' + df.columns[x] + '\"=\"' + value + '\"](' + bb +');'
 
@@ -77,7 +94,7 @@ def main():
     #print(resultstring)
     api = overpy.Overpass()
     #print(dict["cafe"])
-    print("Starting query. This may take a while(<200s)...")
+    print("Starting query. This may take a while(<1000s)...")
     try:
         result = api.query(resultstring)
     except:
@@ -105,16 +122,39 @@ def main():
     COLNAMES = ["point_id", "lon", "lat", "infra_score"]
     nodedf = pd.DataFrame(columns = COLNAMES)
 
+    #### F+r Fortschrittsleiste
+    # Try to get Terminal width
+    try:
+        _, terminal_columns = os.popen('stty size', 'r').read().split()
+    except:
+        terminal_columns = 60
     print("Analyzing nodes...")
     # Nodes
     nodes = result.get_nodes()
-    for i in range(len(nodes)):
+    ct = 0
+    for i in progressbar(range(len(nodes)), size=terminal_columns):
 
         n = nodes[i]
         #print(n.tags)
         weight = findWeight(n.tags)
-        nodedf.loc[i] = [n.id, n.lon, n.lat, weight]
+        nodedf.loc[ct] = [n.id, n.lon, n.lat, weight]
+        ct+=1
         #print(n.id, n.lon, n.lat, weight)
+
+    #Berechnung der Infrastrukturwerte der ways
+    print("Analyzing ways(e.g. building outlines)...")
+    #ct = len(nodedf.loc)-1
+    ways = result.get_ways()
+    for i in progressbar(range(len(ways)),size=terminal_columns):
+        w = ways[i]
+
+        nodes = w.get_nodes(resolve_missing=False)
+        N = len(nodes)
+        weight = findWeight(w.tags)
+        for j in range(N):
+            n = nodes[j]
+            nodedf.loc[ct] = [n.id, n.lon, n.lat, weight]
+            ct+=1
 
     #print(df)
 
@@ -141,37 +181,36 @@ def main():
 
     #id, infra_wert, geometry
 
-    #Berechnung der Infrastrukturwerte der ways
-    print("Analyzing ways(e.g. building outlines)...")
-    ways = result.get_ways()
-    weight_list = [0] * len(ways) * 200
-    meta_geo = [None] * len(ways) * 200
-    ct = 0
-    for i in range(len(ways)):
-        w = ways[i]
-        #print(w.tags)
 
-        nodes = w.get_nodes(resolve_missing=False)
-        N = len(nodes)
-        lat_list = [0] * N
-        lon_list = [0] * N
+    # ways = result.get_ways()
+    # weight_list = [0] * len(ways) * 200
+    # meta_geo = [None] * len(ways) * 200
+    # ct = 0
+    # for i in range(len(ways)):
+    #     w = ways[i]
+    #     #print(w.tags)
+    #
+    #     nodes = w.get_nodes(resolve_missing=False)
+    #     N = len(nodes)
+    #     lat_list = [0] * N
+    #     lon_list = [0] * N
 
         # Scheint nicht zu funktionieren -> siehe Discord
-        for j in range(N):
-            n = nodes[j]
-            lat_list[j] = n.lat
-            lon_list[j] = n.lon
-
-
-            #print(n.id, n.lon, n.lat,weight)
-        try:
-            poly_geo = Polygon(zip(lon_list, lat_list))
-            meta_geo[ct] = poly_geo
-            ct+=1
-            weight_list[ct] = findWeight(w.tags)
-        except:
-            print("WARNING: Invalid way-shape fount @id=", w.id, "i=", i , " ignoring...")
-            pass
+        # for j in range(N):
+        #     n = nodes[j]
+        #     lat_list[j] = n.lat
+        #     lon_list[j] = n.lon
+        #
+        #
+        #     #print(n.id, n.lon, n.lat,weight)
+        # try:
+        #     poly_geo = Polygon(zip(lon_list, lat_list))
+        #     meta_geo[ct] = poly_geo
+        #     weight_list[ct] = findWeight(w.tags)
+        #     ct+=1
+        # except:
+        #     print("WARNING: Invalid way-shape fount @id=", w.id, "i=", i , " ignoring...")
+        #     pass
 
         #Alternative Eckpunkte mit Bewertung/Anzahl der Eckpunkte
         # weight_w = findWeight(w.tags)
@@ -186,42 +225,56 @@ def main():
         #     ct+=1
 
     #arrs zuschneiden
-    weight_list = weight_list[:ct]
-    meta_geo = meta_geo[:ct]
-
-    weight_df = pd.DataFrame(weight_list, columns=["infra_score"])
-
-    poly_df = GeoDataFrame(weight_df, crs=crs, geometry=meta_geo)
-    # print("infra_sco")
-    # print(weight_df)
-
-    polysjoin = gpd.sjoin(grid, poly_df, how="left", op='contains')
-    polypivot = pd.pivot_table(polysjoin, index="id", aggfunc={'infra_score': np.sum})
-    # dfpivot.columns = dfpivot.columns.droplevel()
-
-    finalgrid = grid.merge(polypivot, how='left', on="id")
-    #print(finalgrid)
-    insc_nodes = np.array(finalgrid["infra_score"])
-    insc_ways  = np.array(dfgridnew["infra_score"])
-    print("Fun facts:")
-    print("Nodes:")
-    print("\tµ = ", np.mean(np.array(insc_nodes)))
-    print("\tσ = ", np.std (np.array(insc_nodes)))
-    print("Ways:")
-    print("\tµ = ", np.mean(np.array(insc_ways)))
-    print("\tσ = ", np.std (np.array(insc_ways)))
+    # weight_list = weight_list[:ct]
+    # meta_geo = meta_geo[:ct]
+    #
+    # weight_df = pd.DataFrame(weight_list, columns=["infra_score"])
+    #
+    # poly_df = GeoDataFrame(weight_df, crs=crs, geometry=meta_geo)
+    # # print("infra_sco")
+    # # print(weight_df)
+    #
+    # polysjoin = gpd.sjoin(grid, poly_df, how="left", op='contains')
+    # polypivot = pd.pivot_table(polysjoin, index="id", aggfunc={'infra_score': np.sum})
+    # # dfpivot.columns = dfpivot.columns.droplevel()
+    #
+    # finalgrid = grid.merge(polypivot, how='left', on="id")
+    # #print(finalgrid)
+    # insc_ways = np.array(finalgrid["infra_score"])
+    insc = - np.array(dfgridnew["infra_score"])
+    #print(insc_nodes)
+    ct = 0
+    for x in insc:
+        if x==0:
+            ct+=1
+    print("Amount of unaffected cells: " + str(ct) + "/" + str(len(grid)) +
+            "  ( " + str(round(ct/len(grid)*100,2)) + "% )")
+    print("Distribution")
+    # print("Nodes:")
+    print("\tµ = ", np.mean(insc))
+    print("\tσ = ", np.std (insc))
+    # print("Ways:")
+    # print("\tµ = ", np.mean(insc_ways))
+    # print("\tσ = ", np.std (insc_ways))
 
     #normalisieren
-    insc = -insc_nodes - insc_ways
-    insc = insc/np.max(np.abs(insc)) + 1
-    finalgrid["infra_score"] =  insc
+    #insc = -insc_nodes - insc_ways0
+    fac = 0.1
+    #insc = np.log(1 + insc)
+    sgn = np.sign(insc)
+    insc = sgn*(sgn*insc/np.max(np.abs(insc)))**fac + 1
+    dfgridnew["infra_score"] =  insc
 
+    print("After applying a normilization function:" )
+    # print("Nodes:")
+    print("\tµ = ", np.mean(insc))
+    print("\tσ = ", np.std (insc))
     #print("Gebäudeumrisse mit Infrastrukturwert: ")
     #print(poly)
 
     print("Generated grid:")
-    print(finalgrid)
-    finalgrid.to_file(driver="ESRI Shapefile", filename=sys.argv[3])
+    print(dfgridnew)
+    dfgridnew.to_file(driver="ESRI Shapefile", filename=sys.argv[3])
     print("Saved grid to file: " + sys.argv[3])
     print("Finished")
     print("--- "+ str(round((time.time() - start_time), 2)) + " seconds ---")
